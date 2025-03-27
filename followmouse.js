bb94e41f7beacb3e1675e8826b76aa4cb6e8e16d
Jo-a-n <56139906+Jo-a-n@users.noreply.github.com>
2025-03-27 23:22:29 +0200

    initial commit

---
+++ followmouse.js
@@ -0,0 +1,14 @@
+const move = document.getElementById("move");
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    move.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    
+    }, {duration: 800, fill: "forwards"});
+
+    console.log ("I exist");
+
+}
